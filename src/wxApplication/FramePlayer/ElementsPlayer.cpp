bytespiller/sidplaywx

commit fa1f8aef417545337a55044656c0034f347cb407

Implement STIL support and the ScrollingLabel for displaying it
Also:
- Bump the app version to 0.12.0
- Bundle the STIL.txt (from HVSC v81)
- Update bundled Songlengths DB to HVSC v81

src/wxApplication/FramePlayer/ElementsPlayer.cpp
@@ -37,7 +37,7 @@ namespace FrameElements // Static vars
 
 namespace FrameElements // Static functions
 {
-	static wxStaticText* AttachIconLabel(const ThemeData::ThemeImage& themeImage, wxPanel& parent, wxBoxSizer* parentSizer)
+	static wxStaticText* AttachIconLabel(const ThemeData::ThemeImage& themeImage, const std::string& tooltip, wxPanel& parent, wxBoxSizer* parentSizer)
 	{
 		wxBoxSizer* sizerIconLabel = new wxBoxSizer(wxHORIZONTAL);
 		parentSizer->Add(sizerIconLabel, 1, wxEXPAND, 0);
@@ -45,9 +45,41 @@ namespace FrameElements // Static functions
 		const auto& img = UIElements::Util::LoadRasterizedSvg(themeImage.path.c_str(), DpiSize(TEMP_LABEL_ICON_SIZE), themeImage.offset, themeImage.scale);
 		wxStaticBitmap* bitmapIcon = new wxStaticBitmap(&parent, wxID_ANY, *img.get());
 		sizerIconLabel->Add(bitmapIcon, 0, wxEXPAND | wxALL, TEMP_LABEL_BORDER_SIZE);
+		bitmapIcon->SetToolTip(tooltip);
 
 		wxStaticText* labelPtr = new wxStaticText(&parent, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxST_ELLIPSIZE_END);
 		sizerIconLabel->Add(labelPtr, 1, wxALIGN_CENTER_VERTICAL, TEMP_LABEL_BORDER_SIZE);
+
+		return labelPtr;
+	}
+
+	static UIElements::ScrollingLabel* AttachStilIconScrollingLabel(const ThemeData::ThemeImage& themeImage, const ThemeData::ThemedElementData& themedData, const std::string& tooltip, wxPanel& parent, wxBoxSizer* parentSizer, UIElements::ScrollingLabel::TextJustify justify)
+	{
+		wxBoxSizer* sizerIconLabel = new wxBoxSizer(wxHORIZONTAL);
+		parentSizer->Add(sizerIconLabel, 1, wxEXPAND, 0);
+
+		// Create icon
+		const wxColor iconColor = themedData.GetPropertyColor("iconColor");
+		const std::shared_ptr<wxBitmap>& img = UIElements::Util::LoadRasterizedSvg(themeImage.path.c_str(), DpiSize(TEMP_LABEL_ICON_SIZE), themeImage.offset, themeImage.scale, &iconColor);
+		wxStaticBitmap* bitmapIcon = new wxStaticBitmap(&parent, wxID_ANY, *img.get());
+		bitmapIcon->SetToolTip(tooltip);
+
+		// Create label
+		UIElements::ScrollingLabel* labelPtr = new UIElements::ScrollingLabel(&parent, themedData, justify);
+
+		// Attach & layout the elements
+		wxSizerFlags::DisableConsistencyChecks(); // (wxEXPAND | wxALIGN_CENTER_VERTICAL) is effective combination but the check against it is defective. This call disables this check globally (with no way to re-enable it unfortunately).
+
+		if (justify == UIElements::ScrollingLabel::TextJustify::Right)
+		{
+			sizerIconLabel->Add(labelPtr, 1, wxEXPAND | wxALIGN_CENTER_VERTICAL, TEMP_LABEL_BORDER_SIZE);
+			sizerIconLabel->Add(bitmapIcon, 0, wxEXPAND | wxALL, TEMP_LABEL_BORDER_SIZE);
+		}
+		else
+		{
+			sizerIconLabel->Add(bitmapIcon, 0, wxEXPAND | wxALL, TEMP_LABEL_BORDER_SIZE);
+			sizerIconLabel->Add(labelPtr, 1, wxEXPAND | wxALIGN_CENTER_VERTICAL, TEMP_LABEL_BORDER_SIZE);
+		}
 
 		return labelPtr;
 	}
@@ -126,6 +158,7 @@ namespace FrameElements // Player class
 				wxMenu* viewMenu = new wxMenu();
 				viewMenu->AppendCheckItem(static_cast<int>(MenuItemId_Player::StayTopmost), wxString::Format("%s\tAlt+A", Strings::FramePlayer::MENU_ITEM_STAY_TOPMOST));
 				viewMenu->AppendCheckItem(static_cast<int>(MenuItemId_Player::VisualizationEnabled), wxString::Format(Strings::FramePlayer::MENU_ITEM_VISUALIZATION_ENABLED));
+				viewMenu->AppendCheckItem(static_cast<int>(MenuItemId_Player::StilInfoEnabled), wxString::Format(Strings::FramePlayer::MENU_ITEM_STIL_INFO));
 
 				menuBar->Append(viewMenu, Strings::FramePlayer::MENU_VIEW);
 			}
@@ -146,16 +179,16 @@ namespace FrameElements // Player class
 		wxBoxSizer* sizerSongArea = new wxBoxSizer(wxHORIZONTAL);
 		sizerMain->Add(sizerSongArea, 0, wxEXPAND);
 
-		wxBoxSizer* sizerSongInfoLeft = new wxBoxSizer(wxVERTICAL); // Left
+		// Left
+		wxBoxSizer* sizerSongInfoLeft = new wxBoxSizer(wxVERTICAL);
 		sizerSongArea->Add(sizerSongInfoLeft, 1);
 
-		/*wxBoxSizer* sizerSongInfoRight = new wxBoxSizer(wxVERTICAL); // Right -- TODO (unused for now, perhaps STIL can be displayed here...)
-		sizerSongArea->Add(sizerSongInfoRight, 1, wxEXPAND);*/
+		// Right
+		wxBoxSizer* sizerStilRight = new wxBoxSizer(wxVERTICAL);
+		sizerSongArea->Add(sizerStilRight, 1, wxEXPAND);
 
 		// Labels
-		AttachFixedSizeSeparator(DpiSize(0, 4), sizerSongInfoLeft, _parentPanel); // TODO: magic numbers
-
-		labelTitle = AttachIconLabel(themeData.GetImage("icon_music"), _parentPanel, sizerSongInfoLeft);
+		labelTitle = AttachIconLabel(themeData.GetImage("icon_music"), Strings::FramePlayer::TOOLTIP_PSID_TITLE, _parentPanel, sizerSongInfoLeft);
 
 		{
 			wxFont boldFont = labelTitle->GetFont();
@@ -163,15 +196,25 @@ namespace FrameElements // Player class
 			labelTitle->SetFont(boldFont);
 		}
 
-		labelAuthor = AttachIconLabel(themeData.GetImage("icon_author"), _parentPanel, sizerSongInfoLeft);
-		labelCopyright = AttachIconLabel(themeData.GetImage("icon_copyright"), _parentPanel, sizerSongInfoLeft);
+		labelAuthor = AttachIconLabel(themeData.GetImage("icon_author"), Strings::FramePlayer::TOOLTIP_PSID_AUTHOR, _parentPanel, sizerSongInfoLeft);
+		labelCopyright = AttachIconLabel(themeData.GetImage("icon_copyright"), Strings::FramePlayer::TOOLTIP_PSID_COPYRIGHT, _parentPanel, sizerSongInfoLeft);
+
+		// STIL scrolling labels
+		{
+			const ThemeData::ThemedElementData&	theme = themeData.GetThemedElement("StilInfo");
+			labelStilNameTitle = AttachStilIconScrollingLabel(themeData.GetImage("icon_stilnametitle"), theme, Strings::FramePlayer::TOOLTIP_STIL_NAME_TITLE, _parentPanel, sizerStilRight, UIElements::ScrollingLabel::TextJustify::Right);
+			labelStilArtistAuthor = AttachStilIconScrollingLabel(themeData.GetImage("icon_stilartistauthor"), theme, Strings::FramePlayer::TOOLTIP_STIL_ARTIST_AUTHOR, _parentPanel, sizerStilRight, UIElements::ScrollingLabel::TextJustify::Right);
+			labelStilComment = AttachStilIconScrollingLabel(themeData.GetImage("icon_stilcomment"), theme, Strings::FramePlayer::TOOLTIP_STIL_COMMENT, _parentPanel, sizerStilRight, UIElements::ScrollingLabel::TextJustify::Right);
+		}
 
 		// Visualization
 		waveformVisualization = new UIElements::WaveformVisualization(&_parentPanel, themeData.GetThemedElement("WaveformVisualization"));
 		waveformVisualization->Hide();
-		sizerSongInfoLeft->Add(waveformVisualization, 4, wxEXPAND | wxALL, TEMP_LABEL_BORDER_SIZE);
+		waveformVisualization->SetMinClientSize(wxSize(-1, 10));
+		waveformVisualization->SetMaxClientSize(wxSize(-1, 72));
+		sizerMain->Add(waveformVisualization, 1, wxEXPAND | wxALL, TEMP_LABEL_BORDER_SIZE);
 
-		AttachFixedSizeSeparator(DpiSize(0, 10), sizerSongInfoLeft, _parentPanel); // TODO: magic numbers
+		AttachFixedSizeSeparator(DpiSize(0, 10 + 4), sizerMain, _parentPanel); // TODO: magic numbers
 
 		// Sizer below
 		wxBoxSizer* gridSizerPlaybackButtons = new wxBoxSizer(wxHORIZONTAL);
@@ -206,9 +249,9 @@ namespace FrameElements // Player class
 		// Playback modifier button
 		btnPlaybackMod = AttachSimplePlaybackControlButton(themeData.GetImage("btn_eq"), _parentPanel, gridSizerPlaybackButtons);
 
-		// Seekbar area...
-		AttachFixedSizeSeparator(DpiSize(0, 10), sizerMain, _parentPanel); // TODO: magic number
+		AttachFixedSizeSeparator(DpiSize(0, 1 + 4), sizerMain, _parentPanel); // TODO: magic number
 
+		// Seekbar area...
 		wxBoxSizer* sizerSeekbar = new wxBoxSizer(wxHORIZONTAL);
 		sizerMain->Add(sizerSeekbar, 0, wxEXPAND, 0);
 

dev/theme/default/svg/album-fill.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM12 16C14.2133 16 16 14.2133 16 12C16 9.78667 14.2133 8 12 8C9.78667 8 8 9.78667 8 12C8 14.2133 9.78667 16 12 16ZM12 11C12.55 11 13 11.45 13 12C13 12.55 12.55 13 12 13C11.45 13 11 12.55 11 12C11 11.45 11.45 11 12 11Z"></path></svg>

dev/theme/default/svg/chat-quote-fill.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21 3C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H6.455L2 22.5V4C2 3.44772 2.44772 3 3 3H21ZM10.5153 7.4116C8.72825 8.18684 7.5 9.75543 7.5 11.5052C7.5 12.5 7.77658 13.1137 8.29171 13.6605C8.61598 14.0048 9.12905 14.25 9.66558 14.25C10.6321 14.25 11.4156 13.4665 11.4156 12.5C11.4156 11.5795 10.7045 10.8389 9.80236 10.7553C9.64107 10.7403 9.47827 10.7431 9.32317 10.7645L9.32344 10.6729C9.32873 10.2322 9.4223 8.9333 10.9616 8.1004L10.5153 7.4116ZM15.5153 7.4116C13.7283 8.18684 12.5 9.75543 12.5 11.5052C12.5 12.5 12.7766 13.1137 13.2917 13.6605C13.616 14.0048 14.1291 14.25 14.6656 14.25C15.6321 14.25 16.4156 13.4665 16.4156 12.5C16.4156 11.5795 15.7045 10.8389 14.8024 10.7553C14.6411 10.7403 14.4783 10.7431 14.3232 10.7645L14.3234 10.6729C14.3287 10.2322 14.4223 8.9333 15.9616 8.1004L15.5153 7.4116Z"></path></svg>

dev/theme/default/svg/mic-fill.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.9998 1C14.7612 1 16.9998 3.23858 16.9998 6V10C16.9998 12.7614 14.7612 15 11.9998 15C9.23833 15 6.99976 12.7614 6.99976 10V6C6.99976 3.23858 9.23833 1 11.9998 1ZM3.05469 11H5.07065C5.55588 14.3923 8.47329 17 11.9998 17C15.5262 17 18.4436 14.3923 18.9289 11H20.9448C20.4837 15.1716 17.1714 18.4839 12.9998 18.9451V23H10.9998V18.9451C6.82814 18.4839 3.51584 15.1716 3.05469 11Z"></path></svg>

dev/theme/default/theme.xml
@@ -28,6 +28,9 @@
     <Image type="svg" offset="0,0" scale="1.0" name="icon_expandall" path="svg/add-line.svg"/>
     <Image type="svg" offset="0,0" scale="1.0" name="icon_collapseall" path="svg/subtract-line.svg"/>
     <Image type="svg" offset="0,0" scale="1.0" name="icon_scrolltocurrent" path="svg/home-line.svg"/>
+    <Image type="svg" offset="0,0" scale="1.0" name="icon_stilnametitle" path="svg/album-fill.svg"/>
+    <Image type="svg" offset="0,0" scale="1.0" name="icon_stilartistauthor" path="svg/mic-fill.svg"/>
+    <Image type="svg" offset="0,0" scale="1.0" name="icon_stilcomment" path="svg/chat-quote-fill.svg"/>
   </Images>
   <GuiElements>
     <GuiElement name="Seekbar">
@@ -47,6 +50,12 @@
       <Colors>
         <Color type="wxSYS" value="wxSYS_COLOUR_HIGHLIGHT" property="waveColor"/>
         <Color type="Val" value="white" property="gradientTo"/>
+      </Colors>
+    </GuiElement>
+    <GuiElement name="StilInfo">
+      <Colors>
+        <Color type="wxSYS" value="wxSYS_COLOUR_HIGHLIGHT" property="textColor"/>
+        <Color type="wxSYS" value="wxSYS_COLOUR_HIGHLIGHT" property="iconColor"/>
       </Colors>
     </GuiElement>
   </GuiElements>

sidplaywx.code-workspace
@@ -79,7 +79,8 @@
 			"semaphore": "cpp",
 			"charconv": "cpp",
 			"format": "cpp",
-			"span": "cpp"
+			"span": "cpp",
+			"strstream": "cpp"
 		}
 	}
 }

src/HvscSupport/Songlengths.cpp
@@ -1,0 +1,168 @@
+/*
+ * This file is part of sidplaywx, a GUI player for Commodore 64 SID music files.
+ * Copyright (C) 2021-2024 Jasmin Rutic (bytespiller@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+#include "Songlengths.h"
+#include "../Util/Const.h"
+#include "../Util/HelpersGeneral.h"
+#include <sidplayfp/SidTuneInfo.h>
+#include <fstream>
+#include <vector>
+
+static constexpr uint_least8_t MD5_LEN = 32; // Standard length of MD5 hashes is 32 characters.
+
+static constexpr const char CHAR_INI_COMMENT = ';';
+static constexpr const char CHAR_INI_SECTION = '[';
+
+static constexpr const char CHAR_HASH_DURATION_SEPARATOR = '=';
+static constexpr const char CHAR_SUBSONG_DURATION_SEPARATOR = ' ';
+
+static constexpr const char* const INI_SECTION_DATABASE = "[Database]";
+
+// -----------------------------------------------------------
+
+bool Songlengths::TryLoad(const std::wstring& songlengthsMd5Filepath)
+{
+	Unload();
+
+	std::ifstream file(songlengthsMd5Filepath.c_str());
+
+	bool inDatabaseSection = false;
+	std::string line;
+
+	std::string songHvscPath;
+	while (std::getline(file, line))
+	{
+		const char& firstChar = line.front();
+
+		// Ignore if not within Database section(s)
+		if (firstChar == CHAR_INI_SECTION)
+		{
+			inDatabaseSection = line == INI_SECTION_DATABASE;
+			continue;
+		}
+
+		if (!inDatabaseSection)
+		{
+			continue;
+		}
+
+		// Get HVSC path (or skip the comment)
+		if (firstChar == CHAR_INI_COMMENT)
+		{
+			if (line.length() > 2 && line.at(1) == ' ' && line.at(2) == '/')
+			{
+				songHvscPath = line.substr(2);
+			}
+
+			continue;
+		}
+
+		// Ignore if the line doesn't appear to contain MD5 hash and duration
+		if (line.length() > MD5_LEN && line.at(MD5_LEN) != CHAR_HASH_DURATION_SEPARATOR)
+		{
+			continue;
+		}
+
+		// ---------------------------------------------------
+
+		const std::string& md5Value = line.substr(0, MD5_LEN);
+		const std::string& durations = line.substr(MD5_LEN + 1);
+
+		_database[md5Value] = std::move(std::make_unique<HvscInfoRaw>(durations, songHvscPath));
+	}
+
+	file.close();
+
+	return IsLoaded();
+}
+
+void Songlengths::Unload()
+{
+	_database.clear();
+}
+
+bool Songlengths::IsLoaded() const
+{
+	return !_database.empty();
+}
+
+// mm:ss[.SSS]
+//
+// Examples of song length values:
+//
+// 1:02
+//
+// 1:02.5
+//
+// 1:02.500
+//
+uint_least32_t Songlengths::GetDurationMs(const std::string& preformattedDuration)
+{
+	// Convert string duration to integers
+	std::vector<std::string> subsongDurationSliced = Helpers::General::SplitString(preformattedDuration, ':');
+	const std::string& secondsRaw = subsongDurationSliced.back();
+	const bool hasMillis = secondsRaw.find('.') != std::string::npos;
+
+	int minutes = stoi(subsongDurationSliced.front());
+	int seconds = 0;
+	int extraMillis = 0;
+
+	if (hasMillis)
+	{
+		std::vector<std::string> secondsMillis = Helpers::General::SplitString(secondsRaw, '.');
+		seconds = stoi(secondsMillis.front());
+		extraMillis = stoi(secondsMillis.back());
+	}
+	else
+	{
+		seconds = stoi(secondsRaw);
+	}
+
+	// Convert integer duration to milliseconds
+	return (minutes * Const::MILLISECONDS_IN_MINUTE) + (seconds * Const::MILLISECONDS_IN_SECOND) + extraMillis;
+}
+
+Songlengths::HvscInfo Songlengths::GetHvscInfo(const char* tuneMd5, int subsong) const
+{
+	if (_database.empty())
+	{
+		throw std::runtime_error("Database wasn't loaded!");
+	}
+
+	// Sanity checks
+	if (tuneMd5 == 0)
+	{
+		return HvscInfo(); // Tune wasn't loaded.
+	}
+
+	const auto& it = _database.find(tuneMd5);
+	if (it == _database.cend())
+	{
+		return HvscInfo(); // No tune in database.
+	}
+
+	const std::string& durationsRaw = it->second->durations;
+	std::vector<std::string> subsongsDurations = Helpers::General::SplitString(durationsRaw, CHAR_SUBSONG_DURATION_SEPARATOR);
+	if (subsongsDurations.size() < subsong)
+	{
+		return HvscInfo(); // Database doesn't contain this subsong.
+	}
+
+	const std::string& preformattedDuration = subsongsDurations.at(subsong - 1);
+	return HvscInfo(GetDurationMs(preformattedDuration), it->second->hvscPath, tuneMd5);
+}

src/HvscSupport/Songlengths.h
@@ -1,0 +1,79 @@
+/*
+ * This file is part of sidplaywx, a GUI player for Commodore 64 SID music files.
+ * Copyright (C) 2021-2024 Jasmin Rutic (bytespiller@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+#pragma once
+
+// This is used instead of the libsidplayfp's SidDatabase which unfortunately segfaults when rapidly reading songlengths of huge amount of tunes from the Zip files (data from buffer).
+
+#include <sidplayfp/SidTune.h>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+class Songlengths
+{
+public:
+    struct HvscInfo
+    {
+        // Default constructor for dummy/empty response.
+        HvscInfo() = default;
+
+        HvscInfo(uint_least32_t aDuration, const std::string& aHvscPath, const char* aMd5) :
+            duration(aDuration),
+            hvscPath(aHvscPath),
+            md5(aMd5)
+        {
+        }
+
+        const uint_least32_t duration = 0;
+        const std::string hvscPath;
+        const char* const md5 = 0;
+    };
+
+private:
+    struct HvscInfoRaw
+    {
+        HvscInfoRaw() = delete;
+        HvscInfoRaw(const std::string& aDurations, const std::string& aHvscPath) :
+            durations(aDurations),
+            hvscPath(aHvscPath)
+        {
+        }
+
+        const std::string durations;
+        const std::string hvscPath;
+    };
+
+public:
+    Songlengths() = default;
+
+public:
+    bool TryLoad(const std::wstring& songlengthsMd5Filepath);
+    void Unload();
+
+    bool IsLoaded() const;
+
+    static uint_least32_t GetDurationMs(const std::string& preformattedDuration);
+    HvscInfo GetHvscInfo(const char* tuneMd5, int subsong = 1) const;
+
+private:
+    using MD5 = std::string;
+    std::unordered_map<MD5, std::unique_ptr<HvscInfoRaw>> _database;
+};

commit d45efd48c684ea8be4e2e762ba487197f18367f6

Add some top-margin to clear the song area from the menu bar
Fixes a visual regression introduced in the commit fa1f8aef.

src/wxApplication/FramePlayer/ElementsPlayer.cpp
@@ -175,6 +175,7 @@ namespace FrameElements // Player class
 
 		// Sizers
 		wxBoxSizer* sizerMain = new wxBoxSizer(wxVERTICAL);
+		AttachFixedSizeSeparator(wxSize(-1, TEMP_LABEL_BORDER_SIZE * 2), sizerMain, panel); // Add some top-margin to clear the song area from the menubar.
 
 		wxBoxSizer* sizerSongArea = new wxBoxSizer(wxHORIZONTAL);
 		sizerMain->Add(sizerSongArea, 0, wxEXPAND);