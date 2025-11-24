bytespiller/sidplaywx

commit 046ee7ed12381bdee9c9e780f0b0c1a283531d51

Implement sorting of playlist columns (ascending/descending)

src/wxApplication/UIElements/Playlist/Playlist.cpp
@@ -51,6 +51,15 @@ namespace UIElements
 				AutoFitTextColumn(ColumnId::Title);
 			});
 
+			// Handle sorting
+			Bind(wxEVT_DATAVIEW_COLUMN_HEADER_CLICK, [this](const wxDataViewEvent& evt)
+			{
+				if (wxDataViewColumn* const col = evt.GetDataViewColumn())
+				{
+					_SortByColumn(*col);
+				}
+			});
+
 			// Handle the status icon tooltips
 			const int headerHeight = wxRendererNative::Get().GetHeaderButtonHeight(this);
 			GetMainWindow()->Bind(wxEVT_MOTION, [this, iconColumn, headerHeight](const wxMouseEvent& evt)
@@ -145,6 +154,8 @@ namespace UIElements
 
 		PlaylistTreeModelNode& Playlist::AddMainSong(const wxString& title, const wxString& filepath, int defaultSubsong, uint_least32_t duration, const wxString& hvscPath, const char* md5, const wxString& author, const wxString& copyright, PlaylistTreeModelNode::RomRequirement romRequirement, bool playable)
 		{
+			_ResetColumnSortingIndicator();
+
 			// Create item
 			_model.entries.emplace_back(new PlaylistTreeModelNode(nullptr, title, filepath, defaultSubsong, duration, hvscPath, md5, author, copyright, romRequirement, playable));
 
@@ -212,6 +223,8 @@ namespace UIElements
 
 		void Playlist::Clear()
 		{
+			_ResetColumnSortingIndicator();
+
 			_activeItem.Unset();
 
 			// Clear all entries (entries are unique ptrs so they'll be destroyed since the vector is their owner)
@@ -645,6 +658,97 @@ namespace UIElements
 		wxDataViewColumn* Playlist::_AddTextColumn(ColumnId column, const wxString& title, wxAlignment align, int flags)
 		{
 			return AppendTextColumn(title, static_cast<unsigned int>(column), wxDATAVIEW_CELL_INERT, wxCOL_WIDTH_AUTOSIZE, align, flags);
+		}
+
+		void Playlist::_SortByColumn(wxDataViewColumn& viewColumn)
+		{
+			const ColumnId columnId = static_cast<ColumnId>(viewColumn.GetModelColumn());
+			const bool ascending = !(_columnSortState.columnId == columnId  && _columnSortState.ascending);
+			bool sorted = true;
+
+			// Sort the model (main songs only)
+			switch (columnId)
+			{
+				case PlaylistTreeModel::ColumnId::Title:
+				{
+					std::sort(_model.entries.begin(), _model.entries.end(), [&](const PlaylistTreeModelNodePtr& a, const PlaylistTreeModelNodePtr& b)
+					{
+						const int result = a->title.Cmp(b->title);
+						return (ascending) ? result < 0 : result > 0;
+					});
+
+					break;
+				}
+				case PlaylistTreeModel::ColumnId::Duration:
+				{
+					std::sort(_model.entries.begin(), _model.entries.end(), [&](const PlaylistTreeModelNodePtr& a, const PlaylistTreeModelNodePtr& b)
+					{
+						return (ascending) ? a->duration < b->duration : a->duration > b->duration;
+					});
+
+					break;
+				}
+				case PlaylistTreeModel::ColumnId::Author:
+				{
+					std::sort(_model.entries.begin(), _model.entries.end(), [&](const PlaylistTreeModelNodePtr& a, const PlaylistTreeModelNodePtr& b)
+					{
+						const int result = a->author.Cmp(b->author);
+						return (ascending) ? result < 0 : result > 0;
+					});
+
+					break;
+				}
+				case PlaylistTreeModel::ColumnId::Copyright:
+				{
+					std::sort(_model.entries.begin(), _model.entries.end(), [&](const PlaylistTreeModelNodePtr& a, const PlaylistTreeModelNodePtr& b)
+					{
+						// Ensure years like "200?" are sorted as "2000"
+						wxString aCopyright(a->copyright);
+						aCopyright.Replace('?', '0');
+						wxString bCopyright(b->copyright);
+						bCopyright.Replace('?', '0');
+
+						const int result = aCopyright.Cmp(bCopyright);
+						return (ascending) ? result < 0 : result > 0;
+					});
+
+					break;
+				}
+				default:
+					sorted = false; // Unhandled column
+			}
+
+			if (!sorted)
+			{
+				return;
+			}
+
+			// Set canonical new sorting state
+			_columnSortState.ascending = ascending;
+			_columnSortState.columnId = columnId;
+
+			// TODO: ensure the sorting indicator also gets cleared when the items are reordered (next feature)
+			// Update column sorting indicator
+			for (unsigned int i = 0; i < GetColumnCount(); ++i)
+			{
+				GetColumn(i)->SetBitmap(wxNullBitmap);
+			}
+			viewColumn.SetBitmap(*_model.GetPlaylistIcons().GetIconList().at((_columnSortState.ascending) ? PlaylistIconId::SortAscending : PlaylistIconId::SortDescending).bitmap);
+
+			// Notify the parent (we need to refresh the transport buttons state after sorting in case the first/last positions are swapped)
+			GetEventHandler()->AddPendingEvent(wxDataViewEvent(wxEVT_DATAVIEW_COLUMN_SORTED, this, &viewColumn));
+
+			// Notify the wx base control of change
+			_model.Cleared();
+		}
+
+		void Playlist::_ResetColumnSortingIndicator()
+		{
+			_columnSortState = ColumnSortState();
+			for (unsigned int i = 0; i < GetColumnCount(); ++i)
+			{
+				GetColumn(i)->SetBitmap(wxNullBitmap);
+			}
 		}
 
 #ifdef WIN32

src/wxApplication/Config/UIStrings.h
@@ -265,7 +265,7 @@ namespace Strings
 
 		inline constexpr const char* const OPT_SYSTEM_THEME("System theme");
 		inline constexpr const char* const DESC_SYSTEM_THEME("You can enable the experimental dark theme on Windows here.\nDark theme will be improved in the future.");
-		inline constexpr const char* const ITEM_SYSTEM_THEME_AUTO("Auto");
+		inline constexpr const char* const ITEM_SYSTEM_THEME_AUTO("Auto (experimental)");
 		inline constexpr const char* const ITEM_SYSTEM_THEME_LIGHT("Light");
 		inline constexpr const char* const ITEM_SYSTEM_THEME_DARK("Dark (experimental)");
 

src/wxApplication/FramePlayer/ElementsPlayer.cpp
@@ -355,6 +355,9 @@ namespace FrameElements // Player class
 			playlistIcons.RegisterSvgIcon(UIElements::Playlist::PlaylistIconId::SkipShort, themeData.GetImage("icon_skipshort"), Strings::PlaylistTree::SONG_ICON_TOOLTIP_SKIP_SHORT);
 			playlistIcons.RegisterSvgIcon(UIElements::Playlist::PlaylistIconId::RemoveSong, themeData.GetImage("icon_removesong"), Strings::PlaylistTree::SONG_ICON_TOOLTIP_REMOVE_SONG);
 
+			playlistIcons.RegisterSvgIcon(UIElements::Playlist::PlaylistIconId::SortAscending, themeData.GetImage("icon_removeallabove")); // Reusing from RemoveAll/Above/Below context menu icon.
+			playlistIcons.RegisterSvgIcon(UIElements::Playlist::PlaylistIconId::SortDescending, themeData.GetImage("icon_removeallbelow")); // Reusing from RemoveAll/Above/Below context menu icon.
+
 			treePlaylist = new UIElements::Playlist::Playlist(&_parentPanel, playlistIcons, appSettings, wxDV_SINGLE);
 			sizerMain->Add(treePlaylist->GetWxWindow(), 1, wxEXPAND | wxALL, 0);
 

src/wxApplication/FramePlayer/FramePlayer.h
@@ -180,6 +180,7 @@ private:
     bool DoChangePlaylistTrack(const PlaylistTreeModelNode* const targetNode);
 
     void OnButtonPlaybackMod(wxCommandEvent& evt);
+    void OnButtonTuneInfo(wxCommandEvent& evt);
 
     void OnVolumeSlider(wxCommandEvent& evt);
     void OnButtonRepeatMode(wxCommandEvent& evt);
@@ -190,6 +191,7 @@ private:
     void OnTreePlaylistContextMenuOpen(wxDataViewEvent& evt);
     void OnTreePlaylistContextItem(PlaylistTreeModelNode& node, wxCommandEvent& evt);
     void OnTreePlaylistKeyPressed(wxKeyEvent& evt);
+    void OnTreePlaylistColumnSorted(wxDataViewEvent& evt);
     void OnDropFilesFramePlayer(wxDropFilesEvent& evt);
     void OnDropFilesPlaylist(wxDropFilesEvent& evt);
 

src/wxApplication/FramePlayer/FramePlayer_events.cpp
@@ -86,6 +86,14 @@ void FramePlayer::OnButtonPlaybackMod(wxCommandEvent& /*evt*/)
     OpenPlaybackModFrame();
 }
 
+void FramePlayer::OnButtonTuneInfo(wxCommandEvent& /*evt*/)
+{
+    if (_app.GetPlaybackInfo().IsValidSongLoaded())
+    {
+        ShowTuneInfo();
+    }
+}
+
 void FramePlayer::OnVolumeSlider(wxCommandEvent& evt)
 {
     const float newVolume = static_cast<float>(evt.GetInt()) / _ui->sliderVolume->GetMax();
@@ -342,6 +350,11 @@ void FramePlayer::OnTreePlaylistKeyPressed(wxKeyEvent& evt)
 #else
     evt.Skip();
 #endif
+}
+
+void FramePlayer::OnTreePlaylistColumnSorted(wxDataViewEvent& /*evt*/)
+{
+    UpdateUiState();
 }
 
 void FramePlayer::OnDropFilesFramePlayer(wxDropFilesEvent& evt)

src/wxApplication/FramePlayer/FramePlayer_main.cpp
@@ -315,14 +315,9 @@ void FramePlayer::SetupUiElements()
     _ui->btnPrevSubsong->Bind(wxEVT_BUTTON, &OnButtonSubsongPrev, this);
     _ui->btnNextTrack->Bind(wxEVT_BUTTON, &OnButtonTuneNext, this);
     _ui->btnPrevTrack->Bind(wxEVT_BUTTON, &OnButtonTunePrev, this);
+
     _ui->btnPlaybackMod->Bind(wxEVT_BUTTON, &OnButtonPlaybackMod, this);
-    _ui->btnTuneInfo->Bind(wxEVT_BUTTON, [this](const wxCommandEvent& /*evt*/)
-    {
-        if (_app.GetPlaybackInfo().IsValidSongLoaded())
-        {
-            ShowTuneInfo();
-        }
-    });
+    _ui->btnTuneInfo->Bind(wxEVT_BUTTON, &OnButtonTuneInfo, this);
 
     _ui->sliderVolume->Bind(wxEVT_SLIDER, &OnVolumeSlider, this); // Reminder: context menu is bound in the FrameElementsPlayer to keep stuff tidy.
     _ui->btnRepeatMode->Bind(wxEVT_BUTTON, &OnButtonRepeatMode, this);
@@ -333,6 +328,7 @@ void FramePlayer::SetupUiElements()
     _ui->treePlaylist->Bind(wxEVT_DATAVIEW_ITEM_ACTIVATED, &OnTreePlaylistItemActivated, this);
     _ui->treePlaylist->Bind(wxEVT_DATAVIEW_ITEM_CONTEXT_MENU, &OnTreePlaylistContextMenuOpen, this);
     _ui->treePlaylist->Bind(wxEVT_KEY_DOWN, &OnTreePlaylistKeyPressed, this);
+    _ui->treePlaylist->Bind(wxEVT_DATAVIEW_COLUMN_SORTED, &OnTreePlaylistColumnSorted, this);
 
     SubscribeMe(*_ui->btnRepeatMode, UIElements::SignalsRepeatModeButton::SIGNAL_EXTRA_OPTION_SELECTED, [this](int param)
     {

src/wxApplication/UIElements/Playlist/Components/PlaylistIcons.h
@@ -37,11 +37,16 @@ namespace UIElements
 	{
 		enum class PlaylistIconId : int
 		{
+			// Playlist items status indicators
 			NoIcon = -1,
 			DefaultSubsongIndicator,
 			ChipIcon,
 			SkipShort,
-			RemoveSong
+			RemoveSong,
+
+			// Column sorting indicators
+			SortAscending,
+			SortDescending
 		};
 
 		struct Icon

src/wxApplication/UIElements/Playlist/Components/PlaylistModel.h
@@ -159,8 +159,9 @@ private:
 class PlaylistTreeModel: public wxDataViewModel
 {
 public:
-	enum class ColumnId : unsigned int
+	enum class ColumnId : int
 	{
+		Undefined = -1,
 		Icon = 0,
 		Title,
 		Duration,

commit 5d4fe9131f1a686e38d84d087b87109eb8057427

Fix playlist loading speed regression
Regression was introduced in commit 046ee7e

src/wxApplication/UIElements/Playlist/Playlist.cpp
@@ -744,6 +744,11 @@ namespace UIElements
 
 		void Playlist::_ResetColumnSortingIndicator()
 		{
+			if (_columnSortState.columnId == ColumnId::Undefined) [[likely]]
+			{
+				return; // We must skip this to avoid performance penalty for repeated calls when adding lots of items in bulk.
+			}
+
 			_columnSortState = ColumnSortState();
 			for (unsigned int i = 0; i < GetColumnCount(); ++i)
 			{