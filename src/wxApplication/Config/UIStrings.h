326e060457cc08826639d838c6a5ba764a9080be
bytespiller <bytespiller@users.noreply.github.com>
2025-12-25 08:45:36 +0100

    Correct the APP_VERSION_TAG

--- src/wxApplication/Config/UIStrings.h
+++ src/wxApplication/Config/UIStrings.h
@@ -23,7 +23,7 @@
 
 namespace Strings
 {
-	inline constexpr const char* const APP_VERSION_TAG("0.18"); // Reminder: don't forget to increase.
+	inline constexpr const char* const APP_VERSION_TAG("0.18.0"); // Reminder: don't forget to increase. Also, don't forget the final zero for x.y.0 versions.
 
 	namespace FramePlayer
 	{
