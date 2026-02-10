bytespiller/sidplaywx

commit 7a36eca0fcbaa4e0842e56d3e27397533954ec40

Fix crash when removing currently-playing multi-subsong playlist item

src/wxApplication/UIElements/Playlist/Playlist.cpp
@@ -262,25 +262,25 @@ namespace UIElements
 			}
 		}
 
-		void Playlist::Remove(PlaylistTreeModelNode& item)
+		void Playlist::Remove(PlaylistTreeModelNode* item)
 		{
-			void* parent = item.GetParent(); // Can be nullptr if item is a mainsong.
+			void* parent = item->GetParent(); // Remember the parent (or nullptr if no parent) before the item is removed.
 
-			const PlaylistTreeModelNode* const activeSong = GetActiveSong();
-			if (activeSong != nullptr && activeSong->uid == item.uid)
+			PlaylistTreeModelNode* const activeSong = GetActiveSong();
+			if (item->uid == activeSong->uid || (activeSong->type == PlaylistTreeModelNode::ItemType::Subsong && item->uid == activeSong->GetParent()->uid))
 			{
 				_activeItem.Unset();
 			}
 
 			// Find and remove the item from the model (in case of a main song it is removed from the root, in case of a subsong it is removed from its parent main song)
-			const auto it = std::find_if(_model.entries.cbegin(), _model.entries.cend(), [&item](const PlaylistTreeModelNodePtr& qItemNode) { return qItemNode.get() == &item; });
+			const auto it = std::find_if(_model.entries.cbegin(), _model.entries.cend(), [&item](const PlaylistTreeModelNodePtr& qItemNode) { return qItemNode.get() == item; });
 			if (it != _model.entries.cend())
 			{
-				_model.entries.erase(it); // "item" is now nullptr/invalid, do not access it beyond this point.
+				_model.entries.erase(it); // "item" is now invalid (but not nullptr), do not access it beyond this point.
 			}
 
 			// Notify the wx base control of change
-			_model.ItemDeleted(static_cast<wxDataViewItem>(parent), static_cast<wxDataViewItem>(&item));
+			_model.ItemDeleted(static_cast<wxDataViewItem>(parent), static_cast<wxDataViewItem>(item));
 		}
 
 		void Playlist::Clear()

src/wxApplication/FramePlayer/FramePlayer.h
@@ -224,7 +224,7 @@ private:
 
     PlaylistTreeModelNode* DoFindSong(const wxString& query, const PlaylistTreeModelNode& startNode, bool forwardDirection, bool restart);
     void OnFindSong(UIElements::SignalsSearchBar signalId);
-    void DoRemoveSongTreeItem(PlaylistTreeModelNode& node);
+    void DoRemoveSongTreeItem(PlaylistTreeModelNode* node);
     void DoRemoveAllSongTreeItemsAbove(PlaylistTreeModelNode& node);
     void DoRemoveAllSongTreeItemsBelow(PlaylistTreeModelNode& node);
     void DoToggleSubsongBlacklistState(PlaylistTreeModelNode& node);

src/wxApplication/FramePlayer/FramePlayer_events.cpp
@@ -276,7 +276,7 @@ void FramePlayer::OnTreePlaylistContextItem(PlaylistTreeModelNode& node, wxComma
     switch (id)
     {
         case PopupMenuItemId_Playlist::Remove:
-            DoRemoveSongTreeItem(node);
+            DoRemoveSongTreeItem(&node); // "node" is now invalid (but not nullptr)!
             break;
 
         case PopupMenuItemId_Playlist::RemoveAllAbove:
@@ -315,6 +315,8 @@ void FramePlayer::OnTreePlaylistContextItem(PlaylistTreeModelNode& node, wxComma
 			}
             break;
     }
+
+    // Reminder: "node" may be invalid (but not nullptr) by now!
 }
 
 void FramePlayer::OnTreePlaylistKeyPressed(wxKeyEvent& evt)
@@ -329,7 +331,7 @@ void FramePlayer::OnTreePlaylistKeyPressed(wxKeyEvent& evt)
             PlaylistTreeModelNode* node = PlaylistTreeModel::TreeItemToModelNode(item);
             if (node->type == PlaylistTreeModelNode::ItemType::Song)
             {
-                DoRemoveSongTreeItem(*node);
+                DoRemoveSongTreeItem(node); // "node" is now invalid (but not nullptr)!
             }
             else
             {
@@ -914,15 +916,15 @@ PlaylistTreeModelNode* FramePlayer::DoFindSong(const wxString& query, const Play
     }
 }
 
-void FramePlayer::DoRemoveSongTreeItem(PlaylistTreeModelNode& node)
+void FramePlayer::DoRemoveSongTreeItem(PlaylistTreeModelNode* node)
 {
-    const PlaylistTreeModelNode* activeSong = _ui->treePlaylist->GetActiveSong();
-    if (activeSong != nullptr && activeSong->uid == node.uid)
+    PlaylistTreeModelNode* activeSong = _ui->treePlaylist->GetActiveSong();
+    if (activeSong != nullptr && (activeSong->uid == node->uid || (activeSong->type == PlaylistTreeModelNode::ItemType::Subsong && activeSong->GetParent()->uid == node->uid)))
     {
         _app.UnloadActiveTune();
     }
 
-    _ui->treePlaylist->Remove(node);
+    _ui->treePlaylist->Remove(node); // "node" is now invalid (but not nullptr)!
     PadColumnsWidth();
     UpdateUiState(); // To refresh the Next/Prev buttons.
 }
@@ -954,7 +956,7 @@ void FramePlayer::DoRemoveAllSongTreeItemsAbove(PlaylistTreeModelNode& node)
                 activeSong = nullptr;
             }
 
-            _ui->treePlaylist->Remove(*cSong);
+            _ui->treePlaylist->Remove(cSong);
         }
     }
 
@@ -994,7 +996,7 @@ void FramePlayer::DoRemoveAllSongTreeItemsBelow(PlaylistTreeModelNode& node)
                 activeSong = nullptr;
             }
 
-            _ui->treePlaylist->Remove(*cSong);
+            _ui->treePlaylist->Remove(cSong);
         }
     }
 

src/wxApplication/UIElements/Playlist/Playlist.h
@@ -44,7 +44,7 @@ namespace UIElements
 			void AddSubsongs(const std::vector<uint_least32_t>& durations, const std::vector<wxString>& titles, PlaylistTreeModelNode& parent);
 
 			/// @brief Removes a main song or a subsong item.
-			void Remove(PlaylistTreeModelNode& item);
+			void Remove(PlaylistTreeModelNode* item);
 
 			/// @brief Removes all items from the playlist.
 			void Clear();

commit f80295383a2466feb57350cb2a061cf73e205ae4

Fix crash when removing playlist items + bump version
Regression caused by the recent commit 7a36eca

src/wxApplication/Config/UIStrings.h
@@ -23,7 +23,7 @@
 
 namespace Strings
 {
-	inline constexpr const char* const APP_VERSION_TAG("0.19.0"); // Reminder: don't forget to increase. Also, don't forget the final zero for x.y.0 versions.
+	inline constexpr const char* const APP_VERSION_TAG("0.19.1"); // Reminder: don't forget to increase. Also, don't forget the final zero for x.y.0 versions.
 
 	namespace FramePlayer
 	{

src/wxApplication/UIElements/Playlist/Playlist.cpp
@@ -264,12 +264,14 @@ namespace UIElements
 
 		void Playlist::Remove(PlaylistTreeModelNode* item)
 		{
-			void* parent = item->GetParent(); // Remember the parent (or nullptr if no parent) before the item is removed.
+			void* parent = item->GetParent(); // Remember the parent (or nullptr if no parent) here *before* the item is removed.
 
-			PlaylistTreeModelNode* const activeSong = GetActiveSong();
-			if (item->uid == activeSong->uid || (activeSong->type == PlaylistTreeModelNode::ItemType::Subsong && item->uid == activeSong->GetParent()->uid))
+			if (PlaylistTreeModelNode* const activeSong = GetActiveSong())
 			{
-				_activeItem.Unset();
+				if (item->uid == activeSong->uid || (activeSong->type == PlaylistTreeModelNode::ItemType::Subsong && item->uid == activeSong->GetParent()->uid))
+				{
+					_activeItem.Unset();
+				}
 			}
 
 			// Find and remove the item from the model (in case of a main song it is removed from the root, in case of a subsong it is removed from its parent main song)